f866023027a63d538086b9a6a60245bb02d4e2cd
TradingXZJL <zhoujunlin@tradingx.io>
2025-02-10 18:05:00 +0800

    update

--- handle.go
+++ handle.go
@@ -40,16 +40,14 @@ func handlerCommonRest[T any](data []byte, respHeaderMap map[string]string, stat
 	if is2xx(res.StatusCode) {
 		//请求成功，捕获结构体
 		var d T
-		if len(data) == 0 {
-			res.Data = d
-		} else {
+		if len(data) != 0 {
 			err := json.Unmarshal(data, &d)
 			if err != nil {
 				log.Error("rest返回值获取失败", err)
 				return res, err
 			}
-			res.Data = d
 		}
+		res.Data = d
 	} else {
 		var errRes GateErrorRes
 		err := json.Unmarshal(data, &errRes)
