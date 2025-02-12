cbd7119aa9c21eff2ded331e2c9758e8c5865acc
TradingXZJL <zhoujunlin@tradingx.io>
2025-02-12 18:00:17 +0800

    fix unified accounts bug

--- private_rest_res_unified.go
+++ private_rest_res_unified.go
@@ -4,36 +4,34 @@ type PrivateRestUnifiedAccountsRes struct {
 	UserId      int64 `json:"user_id"`      // 用户 ID
 	RefreshTime int64 `json:"refresh_time"` // 最近一次刷新时间
 	Locked      bool  `json:"locked"`       // 账户是否被锁定
-	Balances    struct {
-		UnifiedBalance struct {
-			Available    string `json:"available"`     // 可用额度
-			Freeze       string `json:"freeze"`        // 被锁定的额度
-			Borrowed     string `json:"borrowed"`      // 借入额度
-			NegativeLiab string `json:"negative_liab"` // 负余额借贷
-			//FuturesPosLiab          string `json:"futures_pos_liab"`          // 合约开仓借币(已废弃,待下线字段)
-			Equity string `json:"equity"` // 权益
-			//TotalFreeze             string `json:"total_freeze"`              // 总占用(已废弃,待下线字段)
-			TotalLiab      string `json:"total_liab"`      // 总借款
-			SpotInUse      string `json:"spot_in_use"`     // 现货对冲占用数量
-			Funding        string `json:"funding"`         // 余币宝理财数量
-			FundingVersion string `json:"funding_version"` // 余币宝理财版本号
-		} `json:"unified_balance"`
-		//Total                   string `json:"total"`                     // 折算成 USD 的账户总资产，即所有币种 (available + freeze) * price 之和,(已废弃，待下线字段，用unified_account_total代替)
-		Borrowed                   string `json:"borrowed"`                      // 折算成 USD 的账户总借入数量，即所有币种(不包括点卡)的 borrowed * price 之和
-		TotalInitialMargin         string `json:"total_initial_margin"`          // 总初始保证金
-		TotalMarginBalance         string `json:"total_margin_balance"`          // 总保证金余额
-		TotalMaintenanceMargin     string `json:"total_maintenance_margin"`      // 总维持保证金
-		TotalInitialMarginRate     string `json:"total_initial_margin_rate"`     // 总初始保证金率
-		TotalMaintenanceMarginRate string `json:"total_maintenance_margin_rate"` // 总维持保证金率
-		TotalAvailableMargin       string `json:"total_available_margin"`        // 可用的保证金额度
-		UnifiedAccountTotal        string `json:"unified_account_total"`         // 统一账户总资产
-		UnifiedAccountTotalLiab    string `json:"unified_account_total_liab"`    // 统一账户总借贷
-		UnifiedAccountTotalEquity  string `json:"unified_account_total_equity"`  // 统一账户总权益
-		Leverage                   string `json:"leverage"`                      // 实际杠杆
-		SpotOrderLoss              string `json:"spot_order_loss"`               // 总挂单损失,单位USDT
-		SpotHedge                  bool   `json:"spot_hedge"`                    // 现货对冲状态, true - 启用，false - 未启用
-		UseFunding                 bool   `json:"use_funding"`                   // 是否将余币宝理财资金作为保证金
+	Balances    map[string]struct {
+		Available    string `json:"available"`     // 可用额度
+		Freeze       string `json:"freeze"`        // 被锁定的额度
+		Borrowed     string `json:"borrowed"`      // 借入额度
+		NegativeLiab string `json:"negative_liab"` // 负余额借贷
+		//FuturesPosLiab          string `json:"futures_pos_liab"`          // 合约开仓借币(已废弃,待下线字段)
+		Equity string `json:"equity"` // 权益
+		//TotalFreeze             string `json:"total_freeze"`              // 总占用(已废弃,待下线字段)
+		TotalLiab      string `json:"total_liab"`      // 总借款
+		SpotInUse      string `json:"spot_in_use"`     // 现货对冲占用数量
+		Funding        string `json:"funding"`         // 余币宝理财数量
+		FundingVersion string `json:"funding_version"` // 余币宝理财版本号
 	} `json:"balances"` // 账户余额
+	//Total                   string `json:"total"`                     // 折算成 USD 的账户总资产，即所有币种 (available + freeze) * price 之和,(已废弃，待下线字段，用unified_account_total代替)
+	Borrowed                   string `json:"borrowed"`                      // 折算成 USD 的账户总借入数量，即所有币种(不包括点卡)的 borrowed * price 之和
+	TotalInitialMargin         string `json:"total_initial_margin"`          // 总初始保证金
+	TotalMarginBalance         string `json:"total_margin_balance"`          // 总保证金余额
+	TotalMaintenanceMargin     string `json:"total_maintenance_margin"`      // 总维持保证金
+	TotalInitialMarginRate     string `json:"total_initial_margin_rate"`     // 总初始保证金率
+	TotalMaintenanceMarginRate string `json:"total_maintenance_margin_rate"` // 总维持保证金率
+	TotalAvailableMargin       string `json:"total_available_margin"`        // 可用的保证金额度
+	UnifiedAccountTotal        string `json:"unified_account_total"`         // 统一账户总资产
+	UnifiedAccountTotalLiab    string `json:"unified_account_total_liab"`    // 统一账户总借贷
+	UnifiedAccountTotalEquity  string `json:"unified_account_total_equity"`  // 统一账户总权益
+	Leverage                   string `json:"leverage"`                      // 实际杠杆
+	SpotOrderLoss              string `json:"spot_order_loss"`               // 总挂单损失,单位USDT
+	SpotHedge                  bool   `json:"spot_hedge"`                    // 现货对冲状态, true - 启用，false - 未启用
+	UseFunding                 bool   `json:"use_funding"`                   // 是否将余币宝理财资金作为保证金
 }
 
 type PrivateRestUnifiedTransferableRes struct {
